9f017cbf2ab93f20c86095307fdb7b0b3f27eb80
Lukas-Nilsson <Lukaspnilsson@gmail.com>
2024-04-08 16:40:02 +1000

    large update to parallax code and scoll snapping

--- scripts.js
+++ scripts.js
@@ -16,23 +16,12 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }, {
       threshold: 0.5,
-      rootMargin: '200px' // Adjust the value to load the content slightly earlier
+      rootMargin: '100px' // Adjust the value to load the content slightly earlier
     });
   
     document.querySelectorAll('img[data-src]').forEach(img => {
       observer.observe(img);
     });
   
-    // const applyParallaxEffect = (element) => {
-    //   // Apply the parallax effect only if the image is loaded
-    //   if (element.classList.contains('loaded')) {
-    //     const speed = parseFloat(element.getAttribute('data-speed'));
-    //     const yPos = -(window.scrollY - element.getBoundingClientRect().top) * speed;
-    //     element.style.transform = `translateY(${yPos}px)`;
-    //   }
-    // };
-  
-    // window.addEventListener('scroll', () => {
-    //   document.querySelectorAll('.parallax-container .pc-layer-img, .parallax-container .pc-stars-img, .parallax-container .pc-flyer-img').forEach(applyParallaxEffect);
-    // });
+
   });
