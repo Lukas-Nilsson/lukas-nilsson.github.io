379cab8aede15589ab05818ee124e5503d47e096
Lukas-Nilsson <Lukaspnilsson@gmail.com>
2024-04-11 17:00:58 +1000

    failed implementation

--- scripts.js
+++ scripts.js
@@ -1,30 +1,30 @@
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const img = entry.target;
-          const highQualitySrc = img.dataset.src;
-          img.src = highQualitySrc;
-          img.onload = () => {
-            img.removeAttribute('data-src');
-            img.classList.add('loaded'); // Add a 'loaded' class after the image is loaded
-          };
-          observer.unobserve(entry.target);
-        }
-      });
-    }, {
-      threshold: 0.5,
-      rootMargin: '100px' // Adjust the value to load the content slightly earlier
-    });
+// document.addEventListener('DOMContentLoaded', () => {
+//     const observer = new IntersectionObserver((entries, observer) => {
+//       entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//           const img = entry.target;
+//           const highQualitySrc = img.dataset.src;
+//           img.src = highQualitySrc;
+//           img.onload = () => {
+//             img.removeAttribute('data-src');
+//             img.classList.add('loaded'); // Add a 'loaded' class after the image is loaded
+//           };
+//           observer.unobserve(entry.target);
+//         }
+//       });
+//     }, {
+//       threshold: 0.5,
+//       rootMargin: '100px' // Adjust the value to load the content slightly earlier
+//     });
   
-    document.querySelectorAll('img[data-src]').forEach(img => {
-      observer.observe(img);
-    });
+//     document.querySelectorAll('img[data-src]').forEach(img => {
+//       observer.observe(img);
+//     });
   
 
-  });
+//   });
 
   // let snapDivTop = document.querySelector('.no-scroll-container').getBoundingClientRect().top;
   // let snapDivBottom = document.querySelector('.no-scroll-container').getBoundingClientRect().bottom;
