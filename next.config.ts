ac8d654d7b54e362cfcc2f142618fcba73a30c15
Lukas Nilsson <lukaspnilsson@gmail.com>
2026-04-07 12:16:49 +1000

    chore: Ignore TS and ESLint on Vercel build to prevent silent OOM crashes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // We run ESLint in our Github Action and local CLI, 
+    // disable here to save Vercel build container memory
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // We already run type checking before pushes,
+    // disable here to prevent silent OOM crashes on Vercel
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
